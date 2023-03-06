0557f57d837c492c8bea8daacf075838ba62744d
Sheharyar <sheharyarone@gmail.com>
2023-03-07 02:42:37 +0500

    factPortal page

--- Pages/factPortal.js
+++ Pages/factPortal.js
@@ -1,34 +1,49 @@
 const animals = [
-  { id: 1, name: "cat", image: "src/img/cat.png", audio: "src/audio/cat.mp3" },
-  { id: 2, name: "cow", image: "src/img/cow.png", audio: "src/audio/cow.mp3" },
-  { id: 3, name: "dog", image: "src/img/dog.png", audio: "src/audio/dog.mp3" },
+  {
+    id: 1,
+    name: "cat",
+    image: "../src/img/cat/png",
+    audio: "../src/audio/cat.mp3",
+  },
+  {
+    id: 2,
+    name: "cow",
+    image: "../src/img/cow.png",
+    audio: "../src/audio/cow.mp3",
+  },
+  {
+    id: 3,
+    name: "dog",
+    image: "../src/img/dog.png",
+    audio: "../src/audio/dog.mp3",
+  },
   {
     id: 4,
     name: "mouse",
-    image: "src/img/mouse.png",
-    audio: "src/audio/mouse.mp3",
+    image: "../src/img/mouse.png",
+    audio: "../src/audio/mouse.mp3",
   },
   {
     id: 5,
     name: "parrot",
-    image: "src/img/parrot.png",
-    audio: "src/audio/parrot.mp3",
+    image: "../src/img/parrot.png",
+    audio: "../src/audio/parrot.mp3",
   },
   {
     id: 6,
     name: "rabbit",
-    image: "src/img/rabbit.png",
-    audio: "src/audio/rabbit.mp3",
+    image: "../src/img/rabbit.png",
+    audio: "../src/audio/rabbit.mp3",
   },
   {
     id: 7,
     name: "sparrow",
-    image: "src/img/sparrow.png",
-    audio: "src/audio/sparrow.mp3",
+    image: "../src/img/sparrow.png",
+    audio: "../src/audio/sparrow.mp3",
   },
 ];
 
-let currentAnimalIndex = 0;
+let currentAnimalIndex = 1;
 const animalPicture = document.getElementById("animal-picture");
 const animalSound = document.getElementById("animal-sound");
 
